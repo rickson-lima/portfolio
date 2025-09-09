cc1e23f2ce33d71be447efb72e7fc2d5314d065e
Rickson Lima <rickson.lima.dev@gmail.com>
2025-09-09 01:08:39 -0400

    Add Vercel configuration, update build settings, and create .vercelignore file

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+  base: "/",
 });
